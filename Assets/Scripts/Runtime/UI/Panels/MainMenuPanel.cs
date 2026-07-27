41783f9b8352ada40ebfc45ae373e1b9a1d6b938
dgdfurkan <frkngndz60@gmail.com>
2026-07-27 15:51:20 +0300

    Gameplay Panel added, Start Button function fixed

--- Assets/Scripts/Runtime/UI/Panels/MainMenuPanel.cs
+++ Assets/Scripts/Runtime/UI/Panels/MainMenuPanel.cs
@@ -68,7 +68,7 @@ namespace Runtime.UI.Panels
 
             DOVirtual.DelayedCall(0.15f, () =>
             {
-                UIManager.Instance.OpenPanel<LanguagePanel>();
+                UIManager.Instance.SwitchToPanel<GameplayPanel>();
             }).SetUpdate(true);
         }
 
